9a9403ca84bc973f0298a3a3a4b914d15d58b8d1
Aaron Redepenning <Aaron Redepenning>
2016-03-23 17:52:58 -0500

    Added Home Page

---
+++ src/app/components/home/home.ts
@@ -0,0 +1,14 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'home',
+  templateUrl: 'app/components/home/home.html',
+  styleUrls: ['app/components/home/home.css']
+})
+
+export class Home {
+
+  constructor() {}
+
+}
--- src/app/dashboard-app.ts
+++ src/app/dashboard-app.ts
@@ -1,21 +1,21 @@
 import {Component} from 'angular2/core';
-//import {Router, RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {Router, RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 
-import { Navbar } from './components/navbar/navbar';
-import { Sidebar } from './components/sidebar/sidebar';
+import { Home } from './components/home/home';
 
 @Component({
   selector: 'dashboard-app',
   providers: [],
   pipes: [],
-  //directives: [ROUTER_DIRECTIVES],
+  directives: [ROUTER_DIRECTIVES],
   templateUrl: 'app/dashboard-app.html',
+  styleUrls: ['app/dashboard-app.css']
 })
-//@RouteConfig([
-  //{ path: '/home',       component: Home,        name: 'Home', useAsDefault: true },
+@RouteConfig([
+  { path: '/home',       component: Home,        name: 'Home', useAsDefault: true },
   //{ path: '/about',      component: About,       name: 'About' },
   //{ path: '/github/...', component: RepoBrowser, name: 'RepoBrowser' },
-//])
+])
 export class DashboardApp {
 
   constructor() {}
