3bdbdfcbc6c0978aff2f51298099692a635f00a3
Aaron Redepenning <Aaron Redepenning>
2016-05-04 13:50:49 -0500

    Changed message parsing

--- src/app/services/fluxmap-service.ts
+++ src/app/services/fluxmap-service.ts
@@ -7,14 +7,14 @@ import { Observable } from 'rxjs/Observable';
  * FluxmapService
  */
 export class FluxmapService {
-    private _fluxmapDataUrl = 'http://10.0.0.178:3000/fluxmap-data';
+    private _fluxmapDataUrl = 'http://localhost:3000/fluxmap-data';
     
     constructor(private http: Http) { }
     
     getFluxmapData(): Observable<any> {
         console.log('Getting data for fluxmap');
         return this.http.get(this._fluxmapDataUrl)
-            .map(this.extractData)
+            .map(res => res.json())
             .catch(this.handleError);
     }
     
@@ -23,7 +23,7 @@ export class FluxmapService {
             throw new Error('Bad response status: ' + res.status);
         }
         let body = res.json();
-        return body.data || { };
+        return body || { };
     }
     
     private handleError(error: any) {
